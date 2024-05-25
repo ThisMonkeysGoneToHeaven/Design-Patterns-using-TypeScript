04067cdc93ec48c8900e6a4e372ea994ce776040
Anant Agarwal <anantagarwal@Anants-MacBook-Air.local>
2024-05-25 06:52:51 +0530

    added an explanation

--- src/Decorator/Condiment/CondimentDecorator.ts
+++ src/Decorator/Condiment/CondimentDecorator.ts
@@ -2,6 +2,12 @@ import Beverage from "../Beverage/Beverage";
 
 abstract class CondimentDecorator extends Beverage{
     
+    /*
+    Why is it necessary for CondimentDecorator to make sure that all its concreteDecoratos implement their own getDescription() ?
+
+    Because, conreteDecorators are inhereting from Beverage which has it's own getDescription() and if we don't make it mandatory, concreteDecorators will depend on Beverage's getDescription() for the calls made to them.
+    */
+
     abstract getDescription(): string;
 }
 
