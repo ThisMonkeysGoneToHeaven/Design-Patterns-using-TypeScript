f61d4802e499090e352c6f58445cc8b3c0ae38f0
ThisMonkeysGoneToHeaven <anantdiagarwal@gmail.com>
2024-06-05 13:52:19 +0530

    added typescript implementation for basic caching decorator

---
+++ src/CachingDecorator/app.ts
@@ -0,0 +1,53 @@
+import express from 'express'
+
+// app config
+const app = express();
+const PORT = 3000;
+
+// Defining Interfaces for Cache & CacheEntry
+interface CacheEntry{
+    expiryTime: number,
+    data: Array<{}>
+}
+interface Cache{
+    [key: string]: CacheEntry
+}
+
+// mockAPI config
+const mockAPIURL = `https://665f0f6a1e9017dc16f2a4e8.mockapi.io/api/v1/data`;
+const cache: Cache = {};
+const TIMEOUT = 30; // in seconds
+
+// exposing the /mockAPI endpoint
+app.get("/mockAPI/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  // if the data is in cache
+  if (userId in cache) {
+    const userData = cache[userId];
+    // if the data is not expired
+    if (userData.expiryTime > Date.now()) {
+      console.log("sending out cached data!");
+      return res.send(userData.data);
+    } else delete cache[userId];
+  }
+
+  // fetch new data
+  fetch(mockAPIURL)
+    .then((response) => response.json())
+    .then((data) => {
+      // cache the newly fetched data
+      cache[userId] = {
+        data: data,
+        expiryTime: Date.now() + TIMEOUT * 1000,
+      };
+
+      console.log("sending out newly fetched data!");
+      res.send(cache[userId].data);
+    });
+});
+
+// making app listen
+app.listen(PORT, () => {
+  console.log(`Listening on localhost port ${PORT}!`);
+});
