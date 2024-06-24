24ab329a4e3522ac05eeff35deec283795c0f169
ThisMonkeysGoneToHeaven <anantdiagarwal@gmail.com>
2024-06-24 14:12:33 +0530

    App class implements IApp to fetch directly from the API

--- src/CachingDecorator/app.ts
+++ src/CachingDecorator/app.ts
@@ -1,42 +1,18 @@
-import express from 'express'
-import InMemoryCache from './InMemoryCache';
-import MyCache from './MyCache';
+import IApp from "./IApp";
 
-// app config
-const app = express();
-const PORT = 3000;
-
-// mockAPI config
 const mockAPIURL = `https://665f0f6a1e9017dc16f2a4e8.mockapi.io/api/v1/data`;
-const TIMEOUT = 30; // in seconds
 
-const cache: MyCache = new InMemoryCache(TIMEOUT);
+export default class App implements IApp{
 
-// exposing the /mockAPI endpoint
-app.get("/mockAPI/:userId", (req, res) => {
-  const userId = req.params.userId;
-
-  // if the data is in cache
-  if (cache.contains(userId)) {
-    // if the data is not expired
-    if (!cache.isExpired(userId)) {
-      console.log("sending out cached data!");
-      return res.send(cache.getData(userId));
-    } else cache.delete(userId);
-  }
-
-  // fetch new data
-  fetch(mockAPIURL)
-    .then((response) => response.json())
-    .then((data) => {
-      // cache the newly fetched data
-      cache.add(userId, data);
-      console.log("sending out newly fetched data!");
-      res.send(data);
-    });
-});
-
-// making app listen
-app.listen(PORT, () => {
-  console.log(`Listening on localhost port ${PORT}!`);
-});
+    async getResult(userId: string): Promise<JSON> {
+        const dataFetchedFromAPI = await this.fetchAPI();
+        return dataFetchedFromAPI;
+    }
+    
+    async fetchAPI(): Promise<JSON>{
+        console.log('fetching from the API');
+        const response = await fetch(mockAPIURL);
+        const data = await response.json();
+        return data;
+    };
+}
